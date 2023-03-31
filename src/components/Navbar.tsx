296e6eb9d7cbcb2af787040c6ce6913c9c871ab7
Vis <vt201916384@gmail.com>
2023-03-30 23:10:02 -0400

    outline plan and navbar started

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='container w-full h-24 bg-rose-700 flex flex-row justify-center'>
+            Navbar
+        </div>
+    )
+}
